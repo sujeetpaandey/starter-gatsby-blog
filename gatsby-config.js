de9859fe64c92ffb1eeb0f88c4ec10f2a0151ba6
Sujeet Pandey <sujeet.pandey@dunnhumby.com>
2021-12-23 14:00:19 +0530

    Renamed Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Sujeet Pandey - Blog",
+    description: "Sujeet Pandey - Blog",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
